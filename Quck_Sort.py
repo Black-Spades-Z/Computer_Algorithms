dff733f9cadba0297347141ad1ed8cd8711cb4a0
Black-Spades-Z <atos4ik@gmail.com>
2023-10-14 20:55:32 +0500

    Quick sort is ready

---
+++ Quck_Sort.py
@@ -0,0 +1,47 @@
+
+
+#array = list()
+
+array = [3,4,12,14,43,2,1,33,22,5]
+
+#size = int(input("Enter the size of array"))
+
+size = len(array)
+
+
+# for index in range(size):
+#     number = int(input(f"array[{index}] : "))
+#     array.append(number)
+
+def pivot(array: list, start: int, end: int):
+
+        p = array[end]
+        i = start - 1
+
+
+        for j in range(start, end):
+                if array[j] < p:
+                        i += 1
+                        temp = array[j]
+                        array[j] = array[i]
+                        array[i] = temp
+
+        i += 1
+        j += 1
+        temp = array[j]
+        array[j] = array[i]
+        array[i] = temp
+        return i
+
+def quicksort(array: list, start: int, end: int):
+        if start >= end:
+            return
+        p = pivot(array, start, end)
+
+        print(array)
+        print(p)
+        quicksort(array, start, p - 1)
+        quicksort(array, p + 1, end)
+
+
+quicksort(array, 0, size -1 )
